84af2b30eba21fd3c07c1709e8a153dfc75c3c57
Stexinator <hurricane90@gmx.de>
2026-02-08 10:49:05 +0100

    add weapon and armor traits

--- module/main.js
+++ module/main.js
@@ -2,4 +2,29 @@ Hooks.on('init', () => {
     foundry.utils.mergeObject(game.impmal.config.disciplines, {
         nurglitePowers: 'Nurglite Powers'
     });
+
+    foundry.utils.mergeObject(game.impmal.config.weaponArmourTraits, {
+        accurate: 'Accurate',
+        fast: 'Fast',
+        sanctified: 'Sanctified',
+        storm: 'Storm (X)',
+        twinLinked: 'Twin-Linked',
+        tripleLinked: 'Triple-Linked',
+        vratine: 'Vratine',
+        quadLinked: 'Quad-Linked',
+        haywire: 'Haywire (X)',
+        luminagen: 'Luminagen',
+        transonic: 'Transonic',
+        gyroStabilized: 'Gyro-Stabilized',
+        vespid: 'Vespid',
+        grav: 'Grav',
+        gauss: 'Gauss',
+        phase: 'Phase',
+        tesla: 'Tesla'
+    });
+
+    foundry.utils.mergeObject(game.impmal.config.traitHasValue, {
+        storm: true,
+        haywire: true
+    });
 });
